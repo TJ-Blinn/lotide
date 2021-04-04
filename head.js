d835251f1e841d0023db159861cbaa2edc0d0d54
TJ-Blinn <tjblinn@hotmail.com>
2021-04-04 22:02:26 +0000

    Implement tail

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  //let actual = "";
+  // let expected = "";
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+//console.log(assertEqual);
+
+const head = function (array) {
+  return array[0];
+};
+//////////////TEST CASES
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([undefined]), undefined);
