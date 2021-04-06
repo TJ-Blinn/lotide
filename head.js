c989d094b79e8a34b08061e84b2e2851c70fd91b
TJ-Blinn <tjblinn@hotmail.com>
2021-04-06 01:16:47 +0000

    head.js

--- head.js
+++ head.js
@@ -15,8 +15,9 @@ const head = function (array) {
   return array[0];
 };
 //////////////TEST CASES
+console.log(head([5, 6, 7]), 5);
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([undefined]), undefined);
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
+// assertEqual(head([undefined]), undefined);
