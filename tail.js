19fde6cec8021e3895430570a96dcf94fa155aa2
TJ-Blinn <tjblinn@hotmail.com>
2021-04-07 20:51:36 +0000

    Refactoring tail

--- tail.js
+++ tail.js
@@ -1,12 +1,14 @@
-const assertEqual = function (actual, expected) {
-  //let actual = "";
-  // let expected = "";
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function (actual, expected) {
+//   //let actual = "";
+//   // let expected = "";
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed:  ${actual}  === ${expected}`);
+//   } else if (actual !== expected) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+const assertEqual = require("./assertEqual");
+
 const tail = function (arr) {
   if (arr.length < 2) {
     // if array is 1 element long, return that element
@@ -15,6 +17,7 @@ const tail = function (arr) {
     return arr.slice(1); // if element is > 1, return array and slice out 1st element.
   }
 };
+
 //console.log(tail([1, 2, 3, 4, 5]));
 
 // Test Case 1: Check the returned array elements
